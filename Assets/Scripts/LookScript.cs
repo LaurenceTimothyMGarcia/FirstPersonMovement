b55e5e42fb09a2b624299e088777d0850377516b
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-09-15 18:50:38 -0700

    reference comments in look script

--- Assets/Scripts/LookScript.cs
+++ Assets/Scripts/LookScript.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LookScript : MonoBehaviour
 {
+    //Step 1
     [SerializeField] private float mouseSensitivity = 500f;
-
     [SerializeField] private Transform playerBody;
 
     private float xRotation = 0f;
@@ -13,6 +13,7 @@ public class LookScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Step 4
         //Locks mouse in, add later to show effect
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -20,14 +21,15 @@ public class LookScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Step 1
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //Prevents mouse from going upside down 
+        //Step 3
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Prevents mouse from going upside down 
 
+        //Step 2
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
 
